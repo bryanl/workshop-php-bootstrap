12542e617d3d1f0990295f4d0e12da58d0b1330b
bryanl <bryan@osesm.com>
2015-07-14 12:55:07 -0400

    preparing apache config

--- clickapp/html/post.php
+++ clickapp/html/post.php
@@ -1,12 +1,8 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "apbw3N7elB";
-$dbname = "myapp";
-#$server = $_ENV["MYSQL_HOST"];
-#$user = $_ENV["MYSQL_USER"];
-#$dname = $_ENV["MYSQL_DB"];
-#$password = $_ENV["MYSQL_PASS"];
+
+require_once dirname(__FILE__) . '/../db.php';
+
+$conn = initDB();
 
 $conn = new mysqli($server, $username, $password, $dbname);
 $stmt = $conn->prepare("insert into points (x, y) values(?, ?)");
