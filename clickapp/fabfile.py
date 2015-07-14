3c5edca8ab38d2a655e9950a68bda7bc5b88b195
bryanl <bryan@osesm.com>
2015-07-14 14:25:24 -0400

    adding a fabfile

---
+++ clickapp/fabfile.py
@@ -0,0 +1,81 @@
+from StringIO import StringIO
+from fabric.api import cd, env, run
+from fabric.contrib.files import exists
+from fabric.operations import sudo, put
+
+def host_type():
+    run('uname -s')
+
+def bootstrap():
+    """ bootstraps a new node """
+    if not exists("/app/workshop-php-bootstrap/"):
+        run("mkdir -p /app/")
+        with cd("/app/"):
+            run("git clone git://github.com/bryanl/workshop-php-bootstrap.git")
+
+    set_apache_config()
+
+
+def install_packages():
+    """ Install a bare minimum LAMP stack """
+    run('apt-get install aptitude')
+    run('aptitude update')
+    run('aptitude upgrade')
+    run('aptitude install libapache2-mod-php5 php5 git')
+    run('aptitude install mysql-server')
+
+def deploy():
+    """ Deploy our app by pulling it from github """
+
+    # NOTE: Python's "with" statement is a "context manager".
+    # All the following commands  will be prefixed with "cd /path/to/  && "
+
+    with cd("/app/workshop-php-bootstrap"):
+        # Fetch & Merge from the remote repo
+        run('git pull git://github.com/bryanl/workshop-php-bootstrap.git')
+
+    set_file_permissions()
+
+def set_file_permissions():
+    """ Sets the appropriate read/execute permissions """
+    with cd("/app/workshop-php-bootstrap/"):
+        run("chmod -R 0755 clickapp/html")
+        run("chown -R root:www-data clickapp/html")
+
+def set_apache_config():
+    """ Replace's apache's default config file """
+    # Delete the existing default config file.
+    if exists("/etc/apache2/sites-enabled/000-default.conf"):
+        run("rm /etc/apache2/sites-enabled/000-default.conf")
+
+    with cd("/etc/apache2/sites-enabled/"):
+        run("ln -sf /app/workshop-php-bootstrap/support/apache/000-default.conf .")
+        run("apachectl restart")
+
+def backup():
+    """ Make a tarball snapshot of the deployed codebase """
+    with cd("/app/"):
+        run("tar -czf /root/workshop-php-bootstrap.tgz workshop-php-bootstrap/")
+
+def revert():
+    """ Revert the codebase from a backup """
+    if exists("/root/workshop-php-bootstrap.tgz"):
+        with cd("/app/"):
+            run("tar -xzf /root/workshop-php-bootstrap.tgz")
+
+def db_setup(user='root', password):
+    """ set up a new database """
+
+    cmd = "mysql -u %s -p%s -h localhost < /app/workshop-php-bootstrap/clickapp/db/db.sql" % (user, password)
+    run(cmd)
+
+    cmd = "mysql -u %s -p%s -h localhost myapp < /app/workshop-php-bootstrap/clickapp/db/myapp.sql" % (user, password)
+    run(cmd)
+
+    put(StringIO(
+    '''
+server = localhost
+username = %s
+password = %s
+dbname = myapp
+    ''' % (user, password)), '/app/workshop-php-bootstrap/clickapp/db.ini')
